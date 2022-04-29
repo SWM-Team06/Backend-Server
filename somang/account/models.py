de7a9b6e7e9ffd14dea3fe8c3007585ad44d379c
youngkwon02 <youngkwon02@naver.com>
2022-04-29 14:35:35 +0900

    feat: account models implemented

--- somang/account/models.py
+++ somang/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    team_id = models.AutoField(primary_key=True)
+    team_name = models.TextField(null=False)
+    project_name = models.TextField(null=False)
+    description = models.TextField(null=True)
+    account_id = models.TextField(null=False)
+    account_pw = models.TextField(null=False)
+    profile_url = models.TextField(null=True)
