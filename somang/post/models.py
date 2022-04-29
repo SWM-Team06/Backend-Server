a88a1703efba1b5c5f505ca03155792078c1c842
LikeTurtle72 <ohsang1030@naver.com>
2022-04-29 18:12:59 +0900

    feat: upadate post models

--- somang/post/models.py
+++ somang/post/models.py
@@ -5,7 +5,15 @@ from datetime import datetime
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     team_id = models.ForeignKey(
-        "account.Team", related_name="team", on_delete=models.CASCADE, db_column="team_id")
+        "account.Team", related_name="post_team", on_delete=models.CASCADE, db_column="team_id")
     img_url = models.TextField(null=False)
     content = models.TextField(null=True)
     uploaded_at = models.DateTimeField(default=datetime.now(), blank=True)
+
+
+class Like(models.Model):
+    like_id = models.IntegerField(primary_key=True)
+    team_id = models.ForeignKey(
+        "account.Team", related_name="like_team", on_delete=models.CASCADE, db_column="team_id")
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, db_column="post_id")
+
