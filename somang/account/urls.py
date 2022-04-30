35e2048d95f1f3323addb779427ae1d3202846ce
JooYeon <48883581+2JooYeon@users.noreply.github.com>
2022-05-01 01:47:33 +0900

    feat: add path to mypage_get

--- somang/account/urls.py
+++ somang/account/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("user", views.user_POST, name="sign_up"),
     path("login", views.login_POST, name="sign_in"),
+    path("mypage", views.mypage_GET, name="mypage_get"),
 ]
