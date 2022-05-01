742664ff953ee5040b05e52328bcf79138657593
youngkwon02 <youngkwon02@naver.com>
2022-05-01 16:23:12 +0900

    fix: remove unnecessary slash at urls router

--- somang/post/urls.py
+++ somang/post/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("feed", views.feed_GET, name="feed_get"),
-    path('/detail/<int:id>', views.post_details, name="post_details")
+    path('detail/<int:id>', views.post_details, name="post_details")
 
 ]
